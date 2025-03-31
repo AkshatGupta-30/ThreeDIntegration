c52990a4adf7eaed20e0bb41946e60e71242bb0e
AkshatGupta-30 <akshatgupta0030@gmail.com>
2025-03-31 15:23:33 +0530

    chore: Initialize project setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,36 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@assets': './src/assets',
+          '@actions': './src/actions',
+          '@components': './src/components',
+          '@config': './src/config',
+          '@constants': './src/constants',
+          '@navigation': './src/navigation',
+          '@network': './src/network',
+          '@screens': './src/screens',
+          '@store': './src/store',
+          '@themes': './src/themes',
+          '@utility': './src/utility',
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'API',
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+  ],
 };
